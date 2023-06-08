24a8f7c1cab584c9c7c76d37a95a86abd6908d1e
spydermnky <peterkhomchenk00@gmail.com>
2023-06-08 12:13:03 +0200

    day6 uploaded

---
+++ week2/day6/libraryhw/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import { useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import BookForm from './components/BookForm';
+import BookTable from './components/BookTable';
+
+function App() {
+  // Declare a new State variable
+  // React hook used managing component state
+  const [books, setBooks] = useState([]);
+  const [bookToEdit, setBookToEdit] = useState(null);
+
+  function onBookCreated(book) {
+    setBookToEdit(null);
+    setBooks([...books, book]);
+  }
+
+  function onBookDelete(book) {
+    setBooks(books.filter((x) => x.isbn !== book.isbn));
+  }
+
+  function onBookEdit(book) {
+    setBookToEdit(book);
+    setBooks(books.filter((x) => x.isbn !== book.isbn));
+  }
+
+  return (
+    <div className="text-center m-5">
+      <div className="card p-4">
+        <BookForm
+          bookToEdit={bookToEdit}
+          onBookCreated={onBookCreated}
+        ></BookForm>
+        <BookTable
+          books={books}
+          onBookEdit={onBookEdit}
+          onBookDelete={onBookDelete}
+        ></BookTable>
+      </div>
+    </div>
+  );
+}
+
+export default App;
